d15606a926a736f66b87326dc25efc0950123ce4
DECIERDO08 <jingleotome@gmail.com>
2025-03-03 12:08:48 -0800

    March 4: yopak -Jingle

--- WebApplication1/Pages/Home.cshtml.cs
+++ WebApplication1/Pages/Home.cshtml.cs
@@ -1,12 +1,17 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace WebApplication1.Pages
 {
     public class HomeModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnPostLogout()
         {
+            HttpContext.SignOutAsync();
+            return RedirectToPage("/Index");
         }
+
     }
 }
