3d92202d2c35012f3d4d560a86c7b2eec1d73432
k <Randulph.Biyok@gmail.com>
2025-03-20 18:56:28 +0800

    Event Calendar

---
+++ WebApplication1/Pages/Event.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class EventModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
